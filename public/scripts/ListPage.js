71627f19c967940085f02b0b47c52d360a14d0bd
Sphesihle Ntshingila <Sphesihle.Ntshingila@bbd.co.za>
2023-05-24 22:19:55 +0200

    New request function definitions

---
+++ public/scripts/ListPage.js
@@ -0,0 +1,14 @@
+const fetchAllSongs = () => {
+  fetch("http://localhost:5000/song/all", {
+    method: "POST",
+    body: JSON.stringify({
+        "username": "admin",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then((response) => console.log(response.json()));
+}
+
+fetchAllSongs();
---
+++ public/scripts/song.js
@@ -0,0 +1,19 @@
+const fetchSong = (songId) => {
+  fetch("http://localhost:5000/song/" + songId, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+    })
+    .then((response) => console.log(response.json()))
+}
+
+const deleteSong = (songId) => {
+  fetch("http://localhost:5000/song/5", {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then((response) => console.log(response.json()))
+}
