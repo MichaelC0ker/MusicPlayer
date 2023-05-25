cd2beded49db457f85604798188ccd4049492186
Benedict Dube <benedict@bbd.co.za>
2023-05-25 22:18:51 +0200

    auth user details

--- music-api/src/controllers/authController.js
+++ music-api/src/controllers/authController.js
@@ -1,17 +1,9 @@
 import httpStatus from 'http-status-codes';
-import { authService } from '../services/authService.js';
+import btoa from 'btoa';
+import * as AuthService from '../services/authService.js';
 import { queryParamExtrator } from '../utils/requestHelper.js';
 
-export default async (req) => {
-  const isUrlContainsCodeParam = req.url.indexOf('code=') >= 0;
-
-  if (!isUrlContainsCodeParam) {
-    // TODO: Handle bad request
-    return {
-      ok: false
-    };
-  }
-
+export const getAccessToken = async (req) => {
   const queryParameters = queryParamExtrator(req.url);
 
   if (!queryParameters || !queryParameters.code) {
@@ -22,11 +14,60 @@ export default async (req) => {
   }
 
   try {
-    const response = await authService(queryParameters.code);
+    const response = await AuthService.getAccessToken(queryParameters.code);
 
     if (!response || !response.ok) {
       const status = response.data.error === 'bad_verification_code'
-        ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR
+        ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
+
+      return {
+        ...response,
+        status: status
+      };
+    }
+
+    return {
+      ok: true,
+      headers: {
+        token: response.data.token
+      }
+    };
+  } catch (err) {
+    return {
+      ok: false,
+      data: {
+        error: err.message ?? 'No error description found'
+      }
+    };
+  }
+};
+
+export const getUserData = async (req) => {
+  const headers = req.headers;
+
+  let token;
+
+  let isValidRequest = !headers || !headers.token || headers.token === '';
+
+  try {
+    token = btoa(headers.token);
+  } catch {
+    isValidRequest = false;
+  }
+
+  if (!isValidRequest) {
+    return {
+      status: httpStatus.BAD_REQUEST,
+      data: 'Failed to get request token from request'
+    };
+  }
+
+  try {
+    const response = await AuthService.getUserData(token);
+
+    if (!response || !response.ok) {
+      const status = response.data.error === 'bad_verification_code'
+        ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
 
       return {
         ...response,
