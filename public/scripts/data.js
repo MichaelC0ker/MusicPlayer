b155e85317c15bbc30c90d70ffad053c9c5527b5
Michael Coker <michaelco@bbd.co.za>
2023-05-26 06:13:27 +0200

    fetching playlists from database now

--- public/scripts/data.js
+++ public/scripts/data.js
@@ -1,13 +1,15 @@
 //store data on front end
 //display data on next screen
 let songs = [];
-const playlistCard = document.querySelector('.playlist-card')
+let playlists =[];
+
+//const playlistCard = document.querySelector('.playlist-card')
 
 async function getAllSongs(){
 	const response = await fetch("http://localhost:5000/song/all", {
     method: "POST",
     body: JSON.stringify({
-          "username": "Tsepo",  
+          "username": "Michael",  
         }),
     headers: {
         "Content-type": "application/json; charset=UTF-8"
@@ -17,4 +19,22 @@ async function getAllSongs(){
 songs = (await response.json()).songs;
 console.log(songs)
 setSongs()
+}
+
+async function getAllPlaylists(){
+    const response = await fetch("http://localhost:5000/playlist/all", {
+    method: "POST",
+    body: JSON.stringify({
+          "username": "Michael"  
+        }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+});
+playlists = (await response.json()).playlists;
+console.log(playlists)
+setPlaylist()
+//playlist = (await response.json()).songs;
+//console.log(songs)
+//setSongs()
 }
