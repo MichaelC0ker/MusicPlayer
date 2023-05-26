6d1103ddb6bfe86ed0d69b8d565ac703c844475c
Benedict Dube <benedict@bbd.co.za>
2023-05-26 03:49:37 +0200

    Change backend endpoint baseurl

--- music-api/src/controllers/authController.js
+++ music-api/src/controllers/authController.js
@@ -4,45 +4,6 @@ import * as AuthService from '../services/authService.js';
 import { queryParamExtrator } from '../utils/requestHelper.js';
 import { addUser } from '../services/userService.js';
 
-export const getAccessToken = async (req) => {
-  const queryParameters = queryParamExtrator(req.url);
-
-  if (!queryParameters || !queryParameters.code) {
-    return {
-      status: httpStatus.BAD_REQUEST,
-      data: 'Failed to get request token from request'
-    };
-  }
-
-  try {
-    const response = await AuthService.getAccessToken(queryParameters.code);
-
-    if (!response || !response.ok) {
-      const status = response.data.error === 'bad_verification_code'
-        ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
-
-      return {
-        ...response,
-        status: status
-      };
-    }
-
-    return {
-      ok: true,
-      headers: {
-        token: response.data.token
-      }
-    };
-  } catch (err) {
-    return {
-      ok: false,
-      data: {
-        error: err.message ?? 'No error description found'
-      }
-    };
-  }
-};
-
 export const getUserData = async (req) => {
   const headers = req.headers;
 
@@ -84,6 +45,40 @@ export const getUserData = async (req) => {
         token: response.data.token
       }
     };
+  } catch (err) {
+    return {
+      ok: false,
+      data: {
+        error: err.message ?? 'No error description found'
+      }
+    };
+  }
+};
+
+export const getAccessToken = async (req) => {
+  const queryParameters = queryParamExtrator(req.url);
+
+  if (!queryParameters || !queryParameters.code) {
+    return {
+      status: httpStatus.BAD_REQUEST,
+      data: 'Failed to get request token from request'
+    };
+  }
+
+  try {
+    const response = await AuthService.getAccessToken(queryParameters.code);
+
+    if (!response || !response.ok) {
+      const status = response.data.error === 'bad_verification_code'
+        ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
+
+      return {
+        ...response,
+        status: status
+      };
+    }
+
+    return await getUserData({ headers: response.data.token });
   } catch (err) {
     return {
       ok: false,
